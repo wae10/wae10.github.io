54cdea347039fdd17d2a6211728c8f9c40cf58b1
wae10 <wae10@georgetown.edu>
2020-11-27 15:16:28 -0600

    project demo

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -186,13 +186,7 @@ let project_mapper = project => {
                     <article class="card__article">
                         <h2><a href="${project.link}">${project.title}</a></h2>
         
-                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `
-                            <a href="${project.demo}">
-                                <i><b>
-                                    GitHub
-                                </b><i/>
-                            </a>` : ''}
-                        </p>
+                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}"><b>GitHub</b></a>` : ''}</p>
                     </article>
 
                                 
