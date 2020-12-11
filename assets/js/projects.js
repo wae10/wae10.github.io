875f7d7fc48e2cf5a97a72950d30e9157de9ae86
wae10 <wae10@georgetown.edu>
2020-12-11 15:08:55 -0600

    storm data file

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -49,7 +49,7 @@ let render_projects = (slug) => {
 
         {
             image: 'assets/images/storm.png',
-            link: ['../assets/excel/storm_funding_assessment.xlsm', '../assets/excel/example_storm_data_residential.xlsm'],
+            link: '../assets/excel/storm',
             title: 'Storm Damage Funding Calc',
             demo: false,
             technologies: ['VBA'],
