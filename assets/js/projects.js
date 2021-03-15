25dce2913ba1ae184ad219f4d0fbbd1522e4ee66
wae10 <wae10@georgetown.edu>
2021-03-15 12:10:33 -0400

    nba tableau addition

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,6 +11,17 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
+
+            image: 'assets/images/nba.png',
+            link: 'https://public.tableau.com/profile/william.everett2026#!/vizhome/DoesWinningintheNBAMatter/NBADashboard?publish=yes',
+            title: 'Does Winning Matter in the NBA?',
+            demo: 'https://github.com/wae10/optimal-portfolio',
+            technologies: ['Tableau', 'Data Visualization','ETL'],
+            description: "Interactive dashboard that allows the user to investigate the question posed in the title. Combined 9 datasets using ETL processes.",
+            categories: ['featured','all', 'tableau']
+        },
+        {
+
             image: 'assets/images/ef.png',
             link: 'https://my-optimal-portfolio.herokuapp.com/',
             title: 'Optimal Portfolio Solver',
